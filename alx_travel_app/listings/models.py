50983424d3e01d757ffd776903a064a9cfe42ddc
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-10-15 16:54:33 +0100

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,1 +1,19 @@
+from django.db import models
+
+class Payment(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Failed', 'Failed'),
+    ]
+
+    booking_reference = models.CharField(max_length=100, unique=True)
+    transaction_id = models.CharField(max_length=200, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.booking_reference} - {self.status}"
 
